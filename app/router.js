09e733abb6c74d4439cac0698dcaf9804037f029
Maciej Wasilewski <wasilewski.maciek@gmail.com>
2021-01-07 14:10:04 +0100

    embroider alias problem

--- app/router.js
+++ app/router.js
@@ -1,10 +1,11 @@
 import EmberRouter from '@ember/routing/router';
 import config from 'emroider-alias/config/environment';
 
+import constants from 'utils/constants'
+
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function() {
-});
+Router.map(function() {});
